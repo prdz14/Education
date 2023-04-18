996223effaf6fb3e5a20b5952e7c0a8d8e1dff36
AminKh <mihakh13@mail.ru>
2023-04-18 13:50:02 +0300

    scriptFirst

--- script.js
+++ script.js
@@ -6,3 +6,29 @@ burger.addEventListener('click', () => {
     list.classList.toggle('active')
 })
 // burger
+
+// count
+class Count {
+    constructor(obj) {
+        const countNumb = document.querySelectorAll('.about_item-title')
+        countNumb.forEach(numb => {
+            const count = +numb.getAttribute('data-num')
+            numb.innerHTML = 0
+            function timer(k = 0) {
+                numb.innerHTML = k
+                k++``
+                if (k <= count) {
+                    setTimeout(() => {
+                        timer(k)
+                    }, 5);
+                }
+            }
+            timer()
+
+        })
+        console.log(countNumb);
+    }
+}
+
+const count = new Count()
+// count
